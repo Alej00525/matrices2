9aca3416d8ff8a8f68f42609e8e139926dc81029
Jose Alejandro Parra Nieto <josealejandroparranieto@gmail.com>
2026-04-27 13:45:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,41 +5,39 @@ import menu
 importlib.reload(ent)
 importlib.reload(mtc)
 importlib.reload(menu)
-def calculadora():
- opcion = 0
- while True:
-  if opcion == 6:
+opcion = 0
+while True:
+ if opcion == 6:
+   break
+ else:
+  print('Elige las matrices:')
+  print('La matriz 1 sera:')
+  matriz1 = ent.ingresar_matriz()
+  print('')
+  print('La martriz 2 sera:')
+  matriz2 = ent.ingresar_matriz()
+  print('')
+  while True:
+   opcion = menu.mostrar_menu()
+   if opcion == 1:
+    print('Resultado:')
+    {ent.imprimir_matriz(mtc.suma_matrices(matriz1,matriz2))}
+    print('')
+   elif opcion == 2:
+    print('Resultado:')
+    {ent.imprimir_matriz(mtc.multiplicacion_matrices(matriz1,matriz2))}
+    print('')
+   elif opcion == 3:
+    print('Resultado:')
+    {ent.imprimir_matriz(mtc.productohadamard_matrices(matriz1,matriz2))}
+    print('')
+   elif opcion == 4:
+    print('Resultado:')
+    {ent.imprimir_matriz(mtc.productokronecker_matrices(matriz1,matriz2))}
+    print('')
+   elif opcion == 5:
+    print('')
     break
-  else:
-   print('Elige las matrices:')
-   print('La matriz 1 sera:')
-   matriz1 = ent.ingresar_matriz()
-   print('')
-   print('La martriz 2 sera:')
-   matriz2 = ent.ingresar_matriz()
-   print('')
-   while True:
-    opcion = menu.mostrar_menu()
-    if opcion == 1:
-     print('Resultado:')
-     {ent.imprimir_matriz(mtc.suma_matrices(matriz1,matriz2))}
-     print('')
-    elif opcion == 2:
-     print('Resultado:')
-     {ent.imprimir_matriz(mtc.multiplicacion_matrices(matriz1,matriz2))}
-     print('')
-    elif opcion == 3:
-     print('Resultado:')
-     {ent.imprimir_matriz(mtc.productohadamard_matrices(matriz1,matriz2))}
-     print('')
-    elif opcion == 4:
-     print('Resultado:')
-     {ent.imprimir_matriz(mtc.productokronecker_matrices(matriz1,matriz2))}
-     print('')
-    elif opcion == 5:
-     print('')
-     print('Elige las nuevas matrices:')
-     break
-    elif opcion == 6:
-     print('Adios!!')
-     break
+   elif opcion == 6:
+    print('Adios!!')
+    break
